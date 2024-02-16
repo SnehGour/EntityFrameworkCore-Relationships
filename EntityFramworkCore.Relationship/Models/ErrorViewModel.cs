4c9315f3741d4eae933fd16232a08123c301b850
snehgour80@gmail.com <snehgour80@gmail.com>
2024-02-16 06:25:14 +0530

    Initial

---
+++ EntityFramworkCore.Relationship/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EntityFramworkCore.Relationship.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
